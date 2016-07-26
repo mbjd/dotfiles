5398c7ac8fd799e0cc2cfe979a63b41f043f55ed
mbjd <balduindettling@gmail.com>
2016-07-26 11:16:03 +0200

    small stuff

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,9 +2,9 @@ function fish_prompt
 	# Print out the exit code if nonzero (errno from moreutils)
 	set -l exit "$status"
 	if test $exit -ne 0
-		printf "\r("
+		printf "\r(exited with "
 		begin
-			errno $exit; or printf "exited with status %s" $exit;
+			errno $exit; or printf "status %s" $exit;
 		end | tr -d '\n'
 		printf ")\n"
 	end
