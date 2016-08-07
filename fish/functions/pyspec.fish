02a0ca4abbc08367273ff950f176e816cbd4cd3b
mbjd <balduindettling@gmail.com>
2016-08-07 17:04:29 +0200

    detrend function for spectrogram

--- fish/functions/pyspec.fish
+++ fish/functions/pyspec.fish
@@ -1,3 +1,3 @@
 function pyspec --description 'Display a spectrogram of newline-separated numbers from stdin'
-	topylist | python -c 'import matplotlib.pyplot as pl; import sys; data = eval(sys.stdin.readlines()[0]); pl.specgram(data); pl.show()'
+	topylist | python -c 'import matplotlib.pyplot as pl; from scipy.signal import detrend; import sys; data = eval(sys.stdin.readlines()[0]); pl.specgram(data, detrend=detrend); pl.show()'
 end
