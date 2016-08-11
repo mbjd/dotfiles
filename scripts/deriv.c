6a0935316f720302e4a64b722f2436b8f2eb9e6e
mbjd <balduindettling@gmail.com>
2016-08-11 16:40:21 +0200

    ran clang-format

--- scripts/deriv.c
+++ scripts/deriv.c
@@ -2,13 +2,14 @@
 
 extern FILE* stdin;
 
-int main()
+int
+main()
 {
 	double old = 0, current = 0;
 	int counter = 0;
 	char line[64];
 
-	while(fgets(line, 64, stdin) != NULL)
+	while (fgets(line, 64, stdin) != NULL)
 	{
 		sscanf(line, "%lf\n", &current);
 		if (counter)
--- scripts/integ.c
+++ scripts/integ.c
@@ -2,13 +2,14 @@
 
 extern FILE* stdin;
 
-int main()
+int
+main()
 {
 	double sum     = 0;
 	double current = 0;
-	char   line[64];
+	char line[64];
 
-	while(fgets(line, 64, stdin) != NULL)
+	while (fgets(line, 64, stdin) != NULL)
 	{
 		sscanf(line, "%lf\n", &current);
 		sum += current;
