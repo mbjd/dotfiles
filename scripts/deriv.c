a50726b95f4fb312980faf4a3a2a88035048f4f7
mbjd <balduindettling@gmail.com>
2016-08-16 20:45:01 +0200

    error handling in integ/deriv

--- scripts/deriv.c
+++ scripts/deriv.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-
-extern FILE* stdin;
+#include <string.h>
 
 int
 main()
 {
 	double old = 0, current = 0;
 	int counter = 0;
+	int ret = 0;
 	char line[64];
 
 	while (fgets(line, 64, stdin) != NULL)
 	{
-		sscanf(line, "%lf\n", &current);
+		line[strcspn(line, "\n")] = '\0'; // Replace first newline by null delimiter
+		ret = sscanf(line, "%lf", &current); // Try to parse one float
+		if (ret == 0)
+		{
+			// If that didn't happen, print an error message
+			fprintf(stderr, "Could not convert '%s' to number\n", line);
+			continue;
+		}
+
 		if (counter)
 		{
-			printf("%f\n", current - old);
+			printf("%lf\n", current - old);
 		}
 		old = current;
 		counter++;
--- scripts/integ.c
+++ scripts/integ.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-
-extern FILE* stdin;
+#include <string.h>
 
 int
 main()
 {
 	double sum     = 0;
 	double current = 0;
+	int ret = 0;
 	char line[64];
 
 	while (fgets(line, 64, stdin) != NULL)
 	{
-		sscanf(line, "%lf\n", &current);
+		line[strcspn(line, "\n")] = '\0'; // Replace first newline by null delimiter
+		ret = sscanf(line, "%lf\n", &current);
+		if (ret != 1)
+		{
+			fprintf(stderr, "Could not convert '%s' to number\n", line);
+			continue;
+		}
 		sum += current;
 		printf("%lf\n", sum);
 	}
