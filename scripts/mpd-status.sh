fd2f3467af2e5384709d5ef512dbcdab694f6e63
mbjd <vir.ingens@gmail.com>
2016-11-05 15:05:08 +0100

    don't print anything if there's no song

--- scripts/mpd-status.sh
+++ scripts/mpd-status.sh
@@ -2,6 +2,9 @@
 # Print a playing/pause indicator and the song name
 
 status=$(mpc status '%name%')
+if test $(echo "$status" | wc -l) -lt 2; then
+	exit
+fi
 
 (
 	echo "$status" | awk 'NR==2 {print}' | sed 's/].*$//g;s/playing/>/g;s/paused/|/g;s/$/] /g'
