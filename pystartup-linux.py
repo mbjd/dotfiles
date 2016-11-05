34e326626a6a3fb1ecc71208eb0fdcec489e35b6
mbjd <vir.ingens@gmail.com>
2016-11-05 14:30:48 +0100

    made python copy() and paste() work

--- pystartup-linux.py
+++ pystartup-linux.py
@@ -5,11 +5,11 @@ import subprocess
 
 def copy(text):
 	# copy a string to the clipboard
-	p = subprocess.Popen(['xclip', 'w'], stdin=subprocess.PIPE, close_fds=True)
+	p = subprocess.Popen(['xclip', '-selection', 'clipboard', '-i'], stdin=subprocess.PIPE, close_fds=True)
 	p.communicate(input=text.encode('utf-8'))
 
 def paste():
 	# paste a string from the clipboard
-	p = subprocess.Popen(['xclip -o', 'r'], stdout=subprocess.PIPE, close_fds=True)
+	p = subprocess.Popen(['xclip', '-selection', 'clipboard', '-o'], stdout=subprocess.PIPE, close_fds=True)
 	stdout, stderr = p.communicate()
 	return stdout.decode('utf-8')
