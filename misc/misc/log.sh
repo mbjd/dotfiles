3122776186f4398457aba47b237179c5769d2e9f
mbjd <vir.ingens@gmail.com>
2016-11-06 20:56:05 +0100

    track battery percentage too

--- misc/misc/log.sh
+++ misc/misc/log.sh
@@ -4,4 +4,4 @@
 
 log_file='/home/balduin/misc/log'
 
-printf '%s,%s\n' $(date +%s) $(/usr/lib/i3blocks/temperature | head -n1 | tr -d '°C') >> "$log_file"
+printf '%s,%s,%s\n' $(date +%s) $(/usr/lib/i3blocks/temperature | head -n1 | tr -d '°C') $(/home/balduin/scripts/batt)>> "$log_file"
