91e257ec5ff14b739427958092111c93c50724bd
Balduin Dettling <balduindettling@gmail.com>
2016-11-25 16:57:07 +0100

    error handling when ffmpeg can't get album art

--- scripts/scripts/refresh-albumart.sh
+++ scripts/scripts/refresh-albumart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Extract the album art of the current song & write to $target
 file="/home/balduin/music/$(mpc --format %file% current)"
+target='/tmp/albumart.jpg'
+yes | ffmpeg -loglevel quiet -i "$file" $target > /dev/null 2> /dev/null
 
-yes | ffmpeg -loglevel quiet -i "$file" /tmp/albumart.jpg > /dev/null 2> /dev/null
+# On failure, fall back to an 'empty album art' image
+status=$?
+[ $status -ne 0 ] || cp /home/balduin/pics/misc/no-albumart.png $target
