781e19ee0ab552dfef457f794671ed5de8bd587e
Balduin Dettling <balduindettling@gmail.com>
2016-11-27 11:11:07 +0100

    better album art script
    now it searches for an image in the album folder if there's no
    embedded album art

--- scripts/scripts/refresh-albumart.sh
+++ scripts/scripts/refresh-albumart.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 # Extract the album art of the current song & write to $target
-file="/home/balduin/music/$(mpc --format %file% current)"
-target='/tmp/albumart.jpg'
-yes | ffmpeg -loglevel quiet -i "$file" $target > /dev/null 2> /dev/null
+# song=$(mpc --format %file% current)
+song=$MPD_SONG_URI
+if [ -n "$song" ]; then
+	file="/home/balduin/music/$song"
+	target='/tmp/albumart.jpg'
+	yes | ffmpeg -loglevel quiet -i "$file" $target > /dev/null 2> /dev/null && exit
+
+	# If that failed, search for an image in the directory of the album
+	cover=$(find $(dirname $file) -type f -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' | head -1)
+	if [ -n "$cover" ]; then
+		cp "$cover" $target
+		exit
+	fi
+fi
 
 # On failure, fall back to an 'empty album art' image
-status=$?
-[ $status -ne 0 ] || cp /home/balduin/pics/misc/no-albumart.png $target
+cp /home/balduin/pics/misc/no-albumart.png $target
