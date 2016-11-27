95b898022399700072f00bde55c8a98b4432851e
Balduin Dettling <balduindettling@gmail.com>
2016-11-27 16:01:39 +0100

    symlink album art instead of copying

--- scripts/scripts/refresh-albumart.sh
+++ scripts/scripts/refresh-albumart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Tries to find the album art for the song currently playing in mpd
+# and copies it to /tmp/albumart.jpg so it can be displayed and stuff
+
 target='/tmp/albumart.jpg'
 
 # Extract the album art of the current song & write to $target
@@ -13,12 +16,11 @@ if [ -n "$song" ]; then
 	cover=$(find $(dirname $file) -type f -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' | head -1)
 	if [ -n "$cover" ]; then
 		# Copy and exit if there is one
-		cp "$cover" $target
-		exit
+		ln -sf "$cover" $target && exit
 	else
 		yes | ffmpeg -loglevel quiet -i "$file" $target > /dev/null 2> /dev/null && exit
 	fi
 fi
 
 # On failure, fall back to an 'empty album art' image
-cp /home/balduin/pics/misc/no-albumart.jpg $target
+ln -sf /home/balduin/pics/misc/no-albumart.jpg $target
