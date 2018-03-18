400a1f84fa289a399c699dcb9fdb5847bc09cbed
Balduin Dettling <balduindettling@gmail.com>
2018-03-18 13:59:17 +0100

    only set wallpaper if that's possible

--- scripts/scripts/bg.sh
+++ scripts/scripts/bg.sh
@@ -5,9 +5,8 @@ bgpath=/home/balduin/pics/desk.jpg
 if [ "$1" = -p -o "$1" = --permanent ]; then
 	shift 1
 	newbg="$1"
+	feh --bg-fill "$newbg" || exit
 	ln -sf "$newbg" $bgpath
-	feh --bg-fill "$newbg"
 else
 	feh --bg-fill "${1:-$bgpath}"
 fi
-
